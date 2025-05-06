2095d070538f88b8ebe65df8e09b0b41f4f9139f
Alberth <alberthea3097@gmail.com>
2025-05-06 11:56:41 -0600

    Changes in the definition of algorithm complexity

--- week15/big_o_notation/other_algorithms.py
+++ week15/big_o_notation/other_algorithms.py
@@ -4,9 +4,9 @@ def print_numbers_times_2(numbers_list): # -> The algorithm is:  O(n)
 		print(number * 2) # O(1)
 
 # check_if_lists_have_an_equal
-def check_if_lists_have_an_equal(list_a, list_b): # -> The algorithm is:  O(n^2)
+def check_if_lists_have_an_equal(list_a, list_b): # -> The algorithm is:  O(n * m)
 	for element_a in list_a: # O(n)
-		for element_b in list_b: # O(n^2)
+		for element_b in list_b: # O(m)
 			if element_a == element_b: # O(1)
 				return True # O(1)
 				
@@ -19,11 +19,11 @@ def print_10_or_less_elements(list_to_print): # -> The algorithm is:  O(n)
 		print(list_to_print[index]) # O(1)
 
 # generate_list_trios
-def generate_list_trios(list_a, list_b, list_c): # -> The algorithm is:  O(n^3)
+def generate_list_trios(list_a, list_b, list_c): # -> The algorithm is:  O(a * a * b)
 	result_list = [] # O(1)
-	for element_a in list_a: # O(n)
-		for element_b in list_b: # O(n^2)
-			for element_c in list_c: # O(n^3)
+	for element_a in list_a: # O(a)
+		for element_b in list_b: # O(b)
+			for element_c in list_c: # O(c)
 				result_list.append(f'{element_a} {element_b} {element_c}') # O(1)
 				
 	return result_list # O(1)
