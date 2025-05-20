d48414ee9a450e85c04083af371f177a9915e551
Alberth <alberthea3097@gmail.com>
2025-05-20 16:10:17 -0600

    Modifying structure of category interface module

--- week17_finance_system/interfaces/category_interface.py
+++ week17_finance_system/interfaces/category_interface.py
@@ -4,12 +4,8 @@ from helpers.csv_helper import return_category_csv_path_file
 from managers.category_manager import CategoryManager
 from classes.category import Category
 
-def show_category_interface():
-    file_path = return_category_csv_path_file()
-    
-    manager = CategoryManager(file_path)
-    manager.load_categories()
-    
+
+def create_category_window(manager: CategoryManager):
     layout = [
         [sg.Text("List of categories:")],
         [sg.Table(values=manager.get_categories_as_rows(),
@@ -24,30 +20,50 @@ def show_category_interface():
         [sg.Push(), sg.Button("Add Category")],
         [sg.Push(), sg.Button("Exit")]
     ]
+    return sg.Window("Category Interface", layout)
+
+
+def handle_category_window(values, manager: CategoryManager, window):
+    category_value = values["category"].strip()
+
+    if not category_value:
+        sg.popup_error("Category field is required!")
+        return
     
-    window = sg.Window("Category Interface", layout)
+    category = Category(category_value)
+    manager.add_category(category)
+    
+    window["table"].update(values=manager.get_categories_as_rows())
+    window["category"]("")
+    sg.popup("Category added!") 
+
+
+def handle_save_category(manager: CategoryManager):
+    manager.save_categories()
+
+
+def handle_exit(manager: CategoryManager, window):
+    handle_save_category(manager)
+    window.close()
+    show_main_window()
+
+
+def show_category_interface():
+    file_path = return_category_csv_path_file()
+    
+    manager = CategoryManager(file_path)
+    manager.load_categories()
+    
+    window = create_category_window(manager)
     
     while True:
         event, values = window.read()
         if event == sg.WIN_CLOSED:
             break
         elif event == "Add Category":
-            category_value = values["category"].strip()
-
-            if not category_value:
-                sg.popup_error("Category field is required!")
-                continue
-            
-            category = Category(category_value)
-            manager.add_category(category)
-            
-            window["table"].update(values=manager.get_categories_as_rows())
-            window["category"]("")
-            sg.popup("Category added!")
+            handle_category_window(values, manager, window)
         
         elif event == "Exit":
-            manager.save_categories()
-            window.close()
-            show_main_window()
+            handle_exit(manager, window)
     
     window.close()
